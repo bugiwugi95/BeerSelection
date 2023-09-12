45582339aa48358661f02c77b36ff02add79e089
илья бугай <bugai.samara@yandex.ru>
2023-09-12 19:23:17 +0400

    Создал свою понель приложения Добавил фрагмент для менюшки и помести ее на понель приложения Стрелку для возврата назад в стартовый фрагмент

--- app/src/main/java/com/hfad/beerselection/MainActivity.kt
+++ app/src/main/java/com/hfad/beerselection/MainActivity.kt
@@ -2,10 +2,46 @@ package com.hfad.beerselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+
 //Ичпользую FragmentContainerView для отображения стартового фрагмента:BeerFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//init свое toolbar и отображение его
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+//Стрелка назад в начальный фрагмент
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val build = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfig = build.build()
+        toolbar.setupWithNavController(navController, appBarConfig)
+
     }
+
+    //добавление меню
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        findNavController(R.id.nav_host_fragment).apply {
+            item.onNavDestinationSelected(this)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
